14eb4dd1e7aead81828cdf1abc47e87153b20c6e
soyotime0118_MacBook <soyotime0118@gmail.com>
2019-11-29 11:51:56 +0900

    initial commit

---
+++ src/test/java/com/example/textparser/TextParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.textparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
