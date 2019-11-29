ec119d5162a3fc32b93b7d8edbbc1c458810639f
soyotime0118_MacBook <soyotime0118@gmail.com>
2019-11-29 13:39:22 +0900

    텍스트 정렬 추가

--- src/main/java/com/example/textparser/parsing/process/TextFilterSortProcessor.java
+++ src/main/java/com/example/textparser/parsing/process/TextFilterSortProcessor.java
@@ -2,7 +2,13 @@ package com.example.textparser.parsing.process;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.joining;
 
 @Service
 public class TextFilterSortProcessor implements TextProcessService {
@@ -14,6 +20,29 @@ public class TextFilterSortProcessor implements TextProcessService {
 
     @Override
     public String sortProcess(String content) {
-        return null;
+        Map<Character, List<Character>> characterListMap = content.chars()
+                .mapToObj(ch -> (char) ch)
+                .collect(
+//                        정렬시 알파벳 순서로 groupby
+                        Collectors.groupingBy(
+//                                key, 대문자
+                                Character::toUpperCase,
+//                                list, 같은 문자끼리도 오름차순 정렬, 대문자->소문자
+                                Collectors.collectingAndThen(
+//                                AAaAA -> AAAAa
+                                        Collectors.toCollection(ArrayList::new),
+                                        l -> {
+                                            l.sort(Character::compareTo);
+                                            return l;
+                                        })
+                        )
+                );
+        String onlySort = characterListMap.keySet()
+                .stream()
+                .sorted()
+                .flatMap(character -> characterListMap.get(character).stream()
+                        .map(character1 -> Character.toString(character1)))
+                .collect(joining());
+        return onlySort;
     }
 }
---
+++ src/test/java/com/example/textparser/parsing/process/TextSortTest.java
@@ -0,0 +1,37 @@
+package com.example.textparser.parsing.process;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+@Log4j2
+class TextSortTest {
+    @InjectMocks
+    TextFilterSortProcessor textFilterSortProcessor;
+
+    @Test
+    void sort_ascend() {
+        String result =textFilterSortProcessor.sortProcess("DCBA");
+        log.info(result);
+        assertThat(result).isEqualTo("ABCD");
+    }
+
+    @Test
+    void sort_ascend_1() {
+        String result =textFilterSortProcessor.sortProcess("DdCBA");
+        log.info(result);
+        assertThat(result).isEqualTo("ABCDd");
+    }
+
+    @Test
+    void sort_ascend_2() {
+        String result =textFilterSortProcessor.sortProcess("0DdaCBA9");
+        log.info(result);
+        assertThat(result).isEqualTo("09AaBCDd");
+    }
+}
